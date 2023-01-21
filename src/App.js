72492aedcd49cf2074f514c970af16844cf06d1f
Beatrice Egumandi <ushiksmandi@gmail.com>
2023-01-21 17:54:25 +0100

    completed task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+
 import './App.css';
 
-function App() {
+const App = () => {
+  const [category, setCategory] = useState('');
+  const [items, setItems] = useState('');
+
+  const foodList = ['Eba', 'Mega pot lover', 'Beans'];
+  const drinkList = ['Hollandia yogurt', 'Monster energy drink'];
+
+  const handleCategoryChange = (e) => {
+    setCategory(e.target.value);
+    if (e.target.value === 'Food') {
+      setItems(foodList);
+    } else if (e.target.value === 'Drink') {
+      setItems(drinkList);
+    } else {
+      console.log('error');
+    }
+  }
+
+  const handleItemsChange = (e) => {
+    setItems(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <form>
+        <label>
+          Category:
+          <select value={category} onChange={handleCategoryChange}>
+            <option value=''>Please select an option</option>
+            <option value='Food'>Food</option>
+            <option value='Drink'>Drink</option>
+          </select>
+        </label>
+        
+        <br />
+      
+        <label>
+          Items:
+          <select value={items} onChange={handleItemsChange}>
+            <option value=''>Please select an option</option>
+            {category === 'Food' ? (foodList.map(item => (
+              <option key={item} value={item}>
+                {item}
+              </option>
+            ))) : (drinkList.map(item => (
+              <option key={item} value={item}>
+                {item}
+              </option>
+            )))}
+          </select>
+        </label>
+
+        <button type='submit'>Submit</button>
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
